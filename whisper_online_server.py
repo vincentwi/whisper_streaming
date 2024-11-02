cefd3d45b8f9d1145f01d20aede5281df9d09636
Jonathan <jonigl@gmail.com>
2024-11-02 11:01:35 +0100

    [apple-sillicon-version] fix: for apple sillicon based machines. WhisperMode device set to cpu. Error  fixed. Code format added.

--- whisper_online_server.py
+++ whisper_online_server.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+import soundfile
+import io
+import logging
+import socket
+import line_packet
 from whisper_online import *
 
 import sys
@@ -14,18 +19,20 @@ parser.add_argument("--port", type=int, default=43007)
 # options from whisper_online
 # TODO: code repetition
 
-parser.add_argument('--min-chunk-size', type=float, default=1.0, help='Minimum audio chunk size in seconds. It waits up to this time to do processing. If the processing takes shorter time, it waits, otherwise it processes the whole segment that was received by this time.')
-parser.add_argument('--model', type=str, default='large-v2', choices="tiny.en,tiny,base.en,base,small.en,small,medium.en,medium,large-v1,large-v2,large".split(","),help="Name size of the Whisper model to use (default: large-v2). The model is automatically downloaded from the model hub if not present in model cache dir.")
+parser.add_argument('--min-chunk-size', type=float, default=1.0,
+                    help='Minimum audio chunk size in seconds. It waits up to this time to do processing. If the processing takes shorter time, it waits, otherwise it processes the whole segment that was received by this time.')
+parser.add_argument('--model', type=str, default='large-v2', choices="tiny.en,tiny,base.en,base,small.en,small,medium.en,medium,large-v1,large-v2,large".split(","),
+                    help="Name size of the Whisper model to use (default: large-v2). The model is automatically downloaded from the model hub if not present in model cache dir.")
 parser.add_argument('--model_cache_dir', type=str, default=None, help="Overriding the default model cache dir where models downloaded from the hub are saved")
 parser.add_argument('--model_dir', type=str, default=None, help="Dir where Whisper model.bin and other files are saved. This option overrides --model and --model_cache_dir parameter.")
 parser.add_argument('--lan', '--language', type=str, default='en', help="Language code for transcription, e.g. en,de,cs.")
-parser.add_argument('--task', type=str, default='transcribe', choices=["transcribe","translate"],help="Transcribe or translate.")
-parser.add_argument('--backend', type=str, default="faster-whisper", choices=["faster-whisper", "whisper_timestamped"],help='Load only this backend for Whisper processing.')
+parser.add_argument('--task', type=str, default='transcribe', choices=["transcribe", "translate"], help="Transcribe or translate.")
+parser.add_argument('--backend', type=str, default="faster-whisper", choices=["faster-whisper", "whisper_timestamped"], help='Load only this backend for Whisper processing.')
 parser.add_argument('--vad', action="store_true", default=False, help='Use VAD = voice activity detection, with the default parameters.')
 args = parser.parse_args()
 
 
-# setting whisper object by args 
+# setting whisper object by args
 
 SAMPLING_RATE = 16000
 
@@ -33,7 +40,7 @@ size = args.model
 language = args.lan
 
 t = time.time()
-print(f"Loading Whisper {size} model for {language}...",file=sys.stderr,end=" ",flush=True)
+print(f"Loading Whisper {size} model for {language}...", file=sys.stderr, end=" ", flush=True)
 
 if args.backend == "faster-whisper":
     from faster_whisper import WhisperModel
@@ -53,38 +60,30 @@ else:
     tgt_language = language
 
 e = time.time()
-print(f"done. It took {round(e-t,2)} seconds.",file=sys.stderr)
+print(f"done. It took {round(e-t,2)} seconds.", file=sys.stderr)
 
 if args.vad:
-    print("setting VAD filter",file=sys.stderr)
+    print("setting VAD filter", file=sys.stderr)
     asr.use_vad()
 
 
 min_chunk = args.min_chunk_size
-online = OnlineASRProcessor(asr,create_tokenizer(tgt_language))
-
+online = OnlineASRProcessor(asr, create_tokenizer(tgt_language))
 
 
 demo_audio_path = "cs-maji-2.16k.wav"
 if os.path.exists(demo_audio_path):
     # load the audio into the LRU cache before we start the timer
-    a = load_audio_chunk(demo_audio_path,0,1)
+    a = load_audio_chunk(demo_audio_path, 0, 1)
 
     # TODO: it should be tested whether it's meaningful
     # warm up the ASR, because the very first transcribe takes much more time than the other
     asr.transcribe(a)
 else:
-    print("Whisper is not warmed up",file=sys.stderr)
+    print("Whisper is not warmed up", file=sys.stderr)
 
 
-
-
-######### Server objects
-
-import line_packet
-import socket
-
-import logging
+# Server objects
 
 
 class Connection:
@@ -113,11 +112,9 @@ class Connection:
         return r
 
 
-import io
-import soundfile
-
-# wraps socket and ASR object, and serves one client connection. 
+# wraps socket and ASR object, and serves one client connection.
 # next client should be served by a new instance of this object
+
 class ServerProcessor:
 
     def __init__(self, c, online_asr_proc, min_chunk):
@@ -138,14 +135,14 @@ class ServerProcessor:
             print(len(raw_bytes))
             if not raw_bytes:
                 break
-            sf = soundfile.SoundFile(io.BytesIO(raw_bytes), channels=1,endian="LITTLE",samplerate=SAMPLING_RATE, subtype="PCM_16",format="RAW")
-            audio, _ = librosa.load(sf,sr=SAMPLING_RATE)
+            sf = soundfile.SoundFile(io.BytesIO(raw_bytes), channels=1, endian="LITTLE", samplerate=SAMPLING_RATE, subtype="PCM_16", format="RAW")
+            audio, _ = librosa.load(sf, sr=SAMPLING_RATE)
             out.append(audio)
         if not out:
             return None
         return np.concatenate(out)
 
-    def format_output_transcript(self,o):
+    def format_output_transcript(self, o):
         # output format in stdout is like:
         # 0 1720 Takhle to je
         # - the first two words are:
@@ -158,15 +155,15 @@ class ServerProcessor:
         # Usually it differs negligibly, by appx 20 ms.
 
         if o[0] is not None:
-            beg, end = o[0]*1000,o[1]*1000
+            beg, end = o[0]*1000, o[1]*1000
             if self.last_end is not None:
                 beg = max(beg, self.last_end)
 
             self.last_end = end
-            print("%1.0f %1.0f %s" % (beg,end,o[2]),flush=True,file=sys.stderr)
-            return "%1.0f %1.0f %s" % (beg,end,o[2])
+            print("%1.0f %1.0f %s" % (beg, end, o[2]), flush=True, file=sys.stderr)
+            return "%1.0f %1.0f %s" % (beg, end, o[2])
         else:
-            print(o,file=sys.stderr,flush=True)
+            print(o, file=sys.stderr, flush=True)
             return None
 
     def send_result(self, o):
@@ -180,20 +177,18 @@ class ServerProcessor:
         while True:
             a = self.receive_audio_chunk()
             if a is None:
-                print("break here",file=sys.stderr)
+                print("break here", file=sys.stderr)
                 break
             self.online_asr_proc.insert_audio_chunk(a)
             o = online.process_iter()
             try:
                 self.send_result(o)
             except BrokenPipeError:
-                print("broken pipe -- connection closed?",file=sys.stderr)
+                print("broken pipe -- connection closed?", file=sys.stderr)
                 break
 
 #        o = online.finish()  # this should be working
 #        self.send_result(o)
-
-
 
 
 # Start logging.
